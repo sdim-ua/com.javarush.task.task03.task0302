4a0319a47e65834add7affb1e6896d0ca92a58dd
sdim-ua <ingersol@yandex.ua>
2019-01-04 10:16:50 +0300

    Create Solution.java

---
+++ Solution.java
@@ -0,0 +1,11 @@
+public class Solution {
+    // напишите тут ваш код
+    
+    public static void printString (String S){
+        System.out.println(S);
+    }
+
+    public static void main(String[] args) {
+        printString("Hello, Amigo!");
+    }
+}
